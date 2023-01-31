b243d54c7bf9176531c83d0545a650423a24d7ed
kavinsaravanan <kavin.saravanan101@gmail.com>
2023-01-31 14:29:46 -0500

    Added approot file to track state

---
+++ src/components/AppRoot.tsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import Nav from './Nav';
+
+export const AppContext = React.createContext<null|any>(null);
+
+export default function AppRoot() {
+
+    // State variables to keep track of the app state globally
+    const [searchQuery, setSearchQuery] = useState<string>('');
+    const [cart, setCart] = useState<any[]>([]);
+    const [course, setCourse] = useState<number>();
+
+
+
+    // Functions to update the states
+
+    function chooseCourse(course: number) {
+        setCourse(course);
+    }
+
+    // Everytime the search query changes, this function will be called
+    function updateSearchQuery(query: string) {
+        setSearchQuery(query);
+    }
+
+    // Removes a course from the cart
+    function removeItemFromCart(id: number) {
+        setCart(cart.filter(item => item.id !== id));
+    }
+
+    function addItemToCart(item: any) {
+        setCart([...cart, item]);
+    }
+
+
+
+    const appContextValue = {
+        chooseCourse,
+        updateSearchQuery,
+        removeItemFromCart,
+        addItemToCart
+    }
+    
+    return (
+        <AppContext.Provider value={appContextValue}>
+            <Nav/>
+        </AppContext.Provider>
+    );
+}
